96a109a9a40d5ffd688452fdc26643dbdafb55f7
Odoo-Canar <odoo@canar.com.sd>
2023-03-19 16:21:59 +0200

    Updates

--- customer_sequence/models/res_partner.py
+++ customer_sequence/models/res_partner.py
@@ -29,20 +29,24 @@ from odoo.exceptions import UserError,ValidationError,AccessError
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    unique_id = fields.Char(string='Unique Id', help="The Unique Sequence no", readonly=True,default='/')
+    unique_id = fields.Char(string='Unique Id', help="The Unique Sequence no",default='/')
+    Abbreviation = fields.Char(string='Abbreviation')
+    delegated_person = fields.Char(string='Delegated Person')
+    sub_account = fields.Integer(string='Sub Account')
 
+    # @api.depends
     @api.model
     def create(self, values):
         res = super(ResPartner, self).create(values)
         company_seq = self.env.company
-        if res.customer_rank >  0 and res.unique_id == '/':
+        if res.customer_rank >  0 :
             if company_seq.next_code:
-                res.unique_id = company_seq.next_code
-                res.name = '[' + str(company_seq.next_code) + ']' + str(res.name)
-                company_seq.write({'next_code': company_seq.next_code + 1})
+                res.unique_id = company_seq.unique_id
+                res.name = '[' + str(company_seq.unique_id) + ']' + str(res.name)
+                company_seq.write({'next_code': company_seq.unique_id + 1})
             else:
-                res.unique_id = company_seq.customer_code
-                res.name = '[' + str(company_seq.customer_code) + ']' + str(res.name)
+                res.unique_id = company_seq.unique_id
+                res.name = '[' + str(company_seq.unique_id) + ']' + str(res.name)
                 company_seq.write({'next_code': company_seq.customer_code + 1})
         return res
 
